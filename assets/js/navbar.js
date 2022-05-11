e3a4c3538fc43a08b46db8b92608468cd0729bfb
mattpsvreis <mateuspsvreis@gmail.com>
2022-05-11 17:14:41 -0300

    Navbar feita

---
+++ assets/js/navbar.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar-menu')
+
+// Display Mobile Menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu);
+
+// Scroll Effect Navbar
+
+const navbar = document.querySelector('.navbar');
+
+const navbarScroll = () => {
+    navbar.classList.toggle("sticky", window.scrollY > 0);
+    menuLinks.classList.toggle("sticky", window.scrollY > 0);
+}
+
+window.addEventListener('scroll', navbarScroll);
